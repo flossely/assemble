7e15e1f1361823df0d622eae24f0aeaf96b6730f
Longinus <jesuscaesar@outlook.com>
2022-03-04 10:13:32 +1000

    Latest upgrade

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <h2 align='center'>Welcome to the Web System Initial Setup</h2>
 <p align='center'>Select the package set you want to install:</p>
 <p align='center'>
+<input type="button" onclick="seq('i,from,assemble,flossely;');" value="Update Assemble">
 <input type="button" onclick="seq('i,from,base,flossely;i,from,basic,flossely;i,from,nixfonts,flossely;i,from,msfonts,flossely;i,from,hsis,flossely;');" value="Base Package">
 <input type="button" onclick="seq('i,from,base,flossely;i,from,basic,flossely;i,from,nixfonts,flossely;i,from,msfonts,flossely;i,from,write,flossely;i,from,delete,flossely;i,from,move,flossely;i,from,copy,flossely;i,from,file,flossely;i,from,files,flossely;i,from,edit,flossely;i,from,font,flossely;i,from,watch,flossely;i,from,hsis,flossely;');" value="Advanced Package">
 <input type="button" onclick="seq('i,from,base,flossely;i,from,basic,flossely;i,from,nixfonts,flossely;i,from,msfonts,flossely;i,from,entity,flossely;i,from,manager,flossely;i,from,hsis,flossely;');" value="Manager Package">
