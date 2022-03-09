f8ee8e5ae008113785c48f13a43b685295313ce4
Longinus <jesuscaesar@outlook.com>
2022-03-09 13:47:21 +1000

    Latest update

--- index.php
+++ index.php
@@ -119,11 +119,11 @@ function seq(data) {
 <h2 align='center'>Welcome to the Web System Initial Setup</h2>
 <p align='center'>Select the package set you want to install:</p>
 <p align='center'>
-<input type="button" onclick="seq('i,,from,assemble,flossely;');" value="Update Assemble">
-<input type="button" onclick="seq('i,,from,base,flossely;i,,from,gnome,flossely;i,,from,nixfonts,flossely;i,,from,msfonts,flossely;i,,from,hsis,flossely;');" value="Base Package">
-<input type="button" onclick="seq('i,,from,base,flossely;i,,from,gnome,flossely;i,,from,nixfonts,flossely;i,,from,msfonts,flossely;i,,from,write,flossely;i,,from,delete,flossely;i,,from,move,flossely;i,,from,copy,flossely;i,,from,file,flossely;i,,from,files,flossely;i,,from,edit,flossely;i,,from,hsis,flossely;');" value="Advanced Package">
-<input type="button" onclick="seq('i,,from,base,flossely;i,,from,gnome,flossely;i,,from,nixfonts,flossely;i,,from,msfonts,flossely;i,,from,entity,flossely;i,,from,manager,flossely;i,,from,hsis,flossely;');" value="Manager Package">
-<input type="button" onclick="seq('i,,from,base,flossely;i,,from,baron,eurohouse;i,,from,nixfonts,flossely;i,,from,msfonts,flossely;i,,from,write,flossely;i,,from,delete,flossely;i,,from,move,flossely;i,,from,copy,flossely;i,,from,file,flossely;i,,from,files,flossely;i,,from,edit,flossely;i,,from,entity,flossely;i,,from,manager,flossely;i,,from,eurohouse,eurohouse;');" value="Premium Package">
+<input type="button" onclick="seq('i,,from,assemble,,flossely;');" value="Update Assemble">
+<input type="button" onclick="seq('i,,from,base,flossely;i,,from,gnome,,flossely;i,,from,nixfonts,,flossely;i,,from,msfonts,,flossely;i,,from,hsis,,flossely;');" value="Base Package">
+<input type="button" onclick="seq('i,,from,base,,flossely;i,,from,gnome,,flossely;i,,from,nixfonts,,flossely;i,,from,msfonts,,flossely;i,,from,write,,flossely;i,,from,delete,,flossely;i,,from,move,,flossely;i,,from,copy,,flossely;i,,from,file,,flossely;i,,from,files,,flossely;i,,from,edit,,flossely;i,,from,hsis,,flossely;');" value="Advanced Package">
+<input type="button" onclick="seq('i,,from,base,,flossely;i,,from,gnome,,flossely;i,,from,nixfonts,,flossely;i,,from,msfonts,,flossely;i,,from,entity,,flossely;i,,from,manager,,flossely;i,,from,hsis,,flossely;');" value="Manager Package">
+<input type="button" onclick="seq('i,,from,base,,flossely;i,,from,baron,,eurohouse;i,,from,nixfonts,,flossely;i,,from,msfonts,,flossely;i,,from,write,,flossely;i,,from,delete,,flossely;i,,from,move,,flossely;i,,from,copy,,flossely;i,,from,file,,flossely;i,,from,files,,flossely;i,,from,edit,,flossely;i,,from,entity,,flossely;i,,from,manager,,flossely;i,,from,eurohouse,,eurohouse;');" value="Premium Package">
 </p>
 </body>
 </html>
